79ea3d6c1d0d56f044ccc3d2fd08381719c6187a
Chris Kapilla <chris@kapilla.net>
2017-11-11 23:34:44 -0600

    add new main.aot.ts and tsconfig.aot.json

---
+++ src/main.aot.ts
@@ -0,0 +1,14 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../aot/src/app/app.component.ngfactory.js';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+console.log('XXXXXXXXin AOT Main.ts with NgFactoryXXXXXXX');
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
+  .catch(err => console.log(err));
