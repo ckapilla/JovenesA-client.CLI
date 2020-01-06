0eec95c0ec198f5db5457af4b7d941c8078c6d40
carloscadena05 <carloscadena05@hotmail.com>
2020-01-06 10:43:05 -0600

    added year and month selector to sponsor's page

--- src/app/_shared/services/url.service.ts
+++ src/app/_shared/services/url.service.ts
@@ -22,7 +22,7 @@ export class UrlService {
       // check ipconfig to verify IPv4 address for current router gateway (usually Wi-Fi)
       //
       // this.webApiPrefix = 'http://192.168.1.100:1100/api/'; // Local Production
-      this.webApiPrefix = 'http://192.168.1.100:1099/api/'; // Local  Development
+      this.webApiPrefix = 'http://192.168.2.33:1099/api/'; // Local  Development
       // this.webApiPrefix = 'https://JAWebAPI-dev.jovenesadelante.org/api/';
       // this.webApiPrefix = 'https://JAWebAPI.jovenesadelante.org/api/';  // Production
       this.clientUrl = 'http://localhost:3000';
--- src/app/sponsors/sponsors-container/sponsors-container.component.ts
+++ src/app/sponsors/sponsors-container/sponsors-container.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { SELECTITEM } from 'src/app/_shared/interfaces/SELECTITEM';
+import { constants } from 'src/app/_shared/constants/constants';
 
 @Component({
   templateUrl: './sponsors-container.component.html'
 })
 export class SponsorsContainerComponent implements OnInit {
   isLoading = false;
-  constructor() { }
+  years: SELECTITEM[];
+  periods: SELECTITEM[];
+  selectedYear: string;
+  selectedPeriod: string;
+  constructor() {
+    this.years = constants.years;
+    this.periods = constants.periods;
+
+    this.selectedYear = '2019'; // '' + today.getFullYear(); //
+    this.selectedPeriod = '4'; // + today.getPeriod() + 1;// '5';
+  }
 
   ngOnInit() {
+  }
+
+  setSelectedYear(year: string) {
+    this.selectedYear = year;
+  }
+  setSelectedPeriod(period: string) {
+    this.selectedPeriod = period;
   }
 
 }
