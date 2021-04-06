031e0956886dc9e6b7445470c5a489c7949ca0d2
Chris Kapilla <chris@kapilla.net>
2021-04-06 15:16:12 -0500

    fix typo in ActiveQRperiods array

--- src/app/_shared/constants/constants.ts
+++ src/app/_shared/constants/constants.ts
@@ -87,7 +87,7 @@ const activeQRperiods = [
   { value: '2020-2', label: '2020 2:Abr-Jun' },
   { value: '2020-3', label: '2020 3:Jul-Set' },
   { value: '2020-4', label: '2020 4:Oct-Dic' },
-  { value: '2020-1', label: '2021 1:Ene-Mar' },
+  { value: '2021-1', label: '2021 1:Ene-Mar' },
 ];
 // moved to UIState const selectedYearPeriod = '2021-1';
 
