a098ab5ce8dffe662a4bf2900f24b28e2c65e4f9
Chris Kapilla <chris@kapilla.net>
2021-06-09 11:08:26 -0500

    add missing url-config.ts file

---
+++ src/app/_shared/services/url-config.ts
@@ -0,0 +1,11 @@
+interface UrlConfig {
+  ProdUrlPrefix: string;
+  DevUrlPrefix: string;
+  localDevUrlPrefix: string;
+}
+
+export const URL_CONFIG: UrlConfig = {
+  ProdUrlPrefix: 'https://JAWebAPI.jovenesadelante.org/',
+  DevUrlPrefix: 'https://JAWebAPI-dev.jovenesadelante.org/',
+  localDevUrlPrefix: 'http://192.168.0.13:1099/'
+};
