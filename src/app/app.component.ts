85848b26e8ae2e9392f57572cd4967279fc79b01
rodriwaw <zapatero.ortiz.luis.rodrigo@gmail.com>
2023-06-27 14:19:43 -0600

    add logos, fix project minor issue with main layout, modify footer to change for students
    changes in this group of files are intented to apply desired style when user is student and to fit elements in page since there was a problem with some layout elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './_shared/services/auth.service';
+import { SessionService } from './_shared/services/session.service';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -10,8 +11,12 @@ import { AuthService } from './_shared/services/auth.service';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(private auth: AuthService) {
+isStudent: boolean = false;
+
+  constructor(private auth: AuthService, private session: SessionService) {
     console.log('AppComponentConstructor, calling localAuthSetup');
     auth.localAuthSetup();
+    this.isStudent= session.isStudent();
+    console.log(this.isStudent)
   }
 }
