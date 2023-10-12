a2ce2971eead1c2257d11ccea2ecbf2fe21e26f3
Chris Kapilla <chris@kapilla.net>
2023-10-11 19:54:38 -0500

    warehouse monnthly date tweaks

--- src/app/_shared/components/whse-daily-mr/whse-daily-mr.component.ts
+++ src/app/_shared/components/whse-daily-mr/whse-daily-mr.component.ts
@@ -28,7 +28,7 @@ export class WHSE_Daily_MR_Component implements OnInit {
     height: 300
   },
   title: {
-      text: 'Daily Mentor Reports Submitted -- Sept 2023'
+      text: 'Daily Mentor Reports Submitted -- Oct 2023'
   },
   // tooltip: {
   //   formatter: function() {
